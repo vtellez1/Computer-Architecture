3df187d105f6a8618cb6b2b21144cccf8f7246ab
Vanessa <vanessatellez45@gmail.com>
2020-05-18 19:16:38 -0400

    Day 1 MVP (steps 0-6)

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,7 +7,9 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.pc = 0
 
     def load(self):
         """Load a program into memory."""
@@ -29,6 +31,16 @@ class CPU:
         for instruction in program:
             self.ram[address] = instruction
             address += 1
+
+
+    def ram_read(self, MAR):
+        #Memory Address Register (MAR)        
+        return self.ram[MAR]
+
+
+    def ram_write(self, MDR, MAR):
+        #Memory Address Register (MAR) and Memory Data Register (MDR)        
+        self.ram[MAR] = MDR
 
 
     def alu(self, op, reg_a, reg_b):
@@ -62,4 +74,34 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        HLT = 0b00000001
+        LDI = 0b10000010
+        PRN = 0b01000111
+
+
+        running = True
+
+        #Instruction Register (IR)
+        IR = self.pc
+
+        while running:
+            instruction = self.ram_read(IR)
+
+            if instruction == LDI:
+                operand_a = self.ram_read(IR +1)
+                operand_b = self.ram_read(IR +2)
+                self.reg[operand_a] = operand_b
+                IR += 3
+
+            elif instruction == PRN:
+                print(self.reg[operand_a])
+                IR += 2
+
+            elif instruction == HLT:
+                running = False
+                sys.exit(0)
+
+            else:
+                print(f'Unknown instruction {instruction} at address {IR}')
+                sys.exit(1)
+
